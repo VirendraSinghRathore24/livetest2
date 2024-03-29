78aed26d8e1c4aae8e5d8429940dcf42f999af3a
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2024-03-29 11:56:58 +0530

    first 5

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(
 	cors({
 		//origin:"http://localhost:3000",
-		origin:"https://virentest.netlify.app/",
+		origin:"https://virentest.netlify.app",
 		credentials:true,
 	})
 )
